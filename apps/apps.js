9fd6c373464c29392a3906b522ecc0bf8d776145
Aaron Rutledge <Aaron.Rutledge@markit.com>
2016-08-12 15:46:29 -0600

    fixing comment typo

--- apps/apps.js
+++ apps/apps.js
@@ -1,5 +1,5 @@
 // This is the entry to our app and acts as a top level container.  This
-//could all bew done just as easily within appClass, but wanted to demonstrate
+//could all be done just as easily within appClass, but wanted to demonstrate
 //ES2015 module imports and I think makes for a nice separation between app stuff
 //and F2 implementation details
 
